fa6af2e0ca192e0d94248dfa00479d9b723c54f8
Austin Rovge <austinrovge@gmail.com>
2022-05-28 23:41:29 -0500

    add resume link

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import ExternalAnchorTag from './externalAnchorTag';
 import * as styles from './header.module.scss';
 
 export default function Header() {
@@ -11,6 +12,7 @@ export default function Header() {
                     <Link to='/'>About</Link>
                     <Link to='/projects'>Projects</Link>
                     <Link to='/photography'>Photography</Link>
+                    <ExternalAnchorTag href='https://api.ajr.dev/files/austin-rovge-resume.pdf'>Resume</ExternalAnchorTag>
                 </nav>
             </div>
         </header>
